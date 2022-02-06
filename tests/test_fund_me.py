7808d7cfc0ab7747bd97a3cc3c7aa5df67022a06
epomatti <evandro.pomatti@gmail.com>
2022-02-06 02:27:40 -0300

    testing

--- tests/test_fund_me.py
+++ tests/test_fund_me.py
@@ -1,5 +1,7 @@
-from scripts.utils import get_account
+from scripts.utils import get_account, LOCAL_BLOCKCHAIN_ENVIRONMENTS
 from scripts.deploy_fundme import FundMe, deploy_fund_me
+from brownie import network, accounts, exceptions
+import pytest
 
 
 def test_can_fund_and_withdraw():
@@ -12,3 +14,12 @@ def test_can_fund_and_withdraw():
     tx2 = fund_me.withdraw({"from": account})
     tx2.wait(1)
     assert fund_me.addressToAmountFunded(account.address) == 0
+
+
+def test_only_owner_can_withdraw():
+    if network.show_active not in LOCAL_BLOCKCHAIN_ENVIRONMENTS:
+        pytest.skip("only for local testing")
+    fund_me = deploy_fund_me()
+    bad_actor = accounts.add()
+    with pytest.raises(exceptions.VirtualMachineError):
+        fund_me.withdraw({"from": bad_actor})
